b7663ea7613f72abe0becd90d1f84b9dd6cdc3bf
Yizi Gu <yizigu@gmail.com>
2021-04-28 01:16:26 -0500

    [misc] add env_setup.sh for reference

--- env_setup.sh
+++ env_setup.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
-module load gflags-2.1.2-gcc-7.4.0-4vasfdn 
-module load glog-0.3.5-gcc-7.4.0-y7ajvq2    
-module load intel-tbb-2020.2-gcc-7.4.0-tpthcp6
-module load boost-1.72.0-gcc-7.4.0-mfchp2d
-module load llvm-openmp-romp-mod-gcc-7.4.0-6kbf57l 
-module load dyninst-10.1.2-gcc-7.4.0-pxqjj4q
-module load gcc-7.4.0-gcc-7.5.0-domzzsx
+module load gcc-10.3.0-gcc-7.4.0-t6c5j3q
+echo 'loaded gcc-10.3.0'
+module load gflags-2.2.2-gcc-10.3.0-svbilh4
+echo 'loaded gflags-2.2.2'
+module load intel-tbb-2020.3-gcc-10.3.0-kpcw75a
+echo 'loaded intel-tbb-2020.3'
+module load boost-1.76.0-gcc-10.3.0-6t5io7m
+echo 'loaded boost-1.76.0'
+module load llvm-openmp-romp-mod-gcc-10.3.0-vccvbtz
+echo 'loaded llvm-openmp-romp-mod'
+module load dyninst-11.0.0-gcc-10.3.0-sfbg3fy
+echo 'loaded dyninst-11.0.0'
+module load gcc-10.3.0-gcc-7.4.0-t6c5j3q
+echo 'loaded gcc-10.3.0'
+module load cmake-3.20.1-gcc-10.3.0-tuzc2ws
+echo 'loaded cmake-3.20'
+module load glog-0.4.0-gcc-10.3.0-srrufcz
+echo 'loaded glog-0.4.0'
 echo "modules loaded"
+
