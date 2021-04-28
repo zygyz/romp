40ec7805b6e7214930ee03b37f884d3ad79fcb16
Yizi Gu <yizigu@gmail.com>
2021-04-28 01:04:17 -0500

    [build] added install.sh and fix CMakeList file

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/bash
-# This script is only used for cmake configuration stage. One needs to load all # required modules before running this script.
+#!/usr/bin/env bash
+rm -rf build
+rm -rf install
+mkdir build
 mkdir install
-mkdir build && cd build
-cmake -DCMAKE_CXX_FLAGS=-std=c++17
-      -DCMAKE_CXX_COMPILER=g++
-      -DCMAKE_C_COMPILER=gcc
-      -DCMAKE_INSTALL_PREFIX=`pwd`/../install ..
-make
+cd build
+cmake -DCMAKE_CXX_FLAGS=-std=c++17 -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=`pwd`/../install ..
+make 
 make install
+cd ..
+
