6306bf42e88bebbba752f0a313ceccde2e26d10b
Yizi Gu <yizigu@gmail.com>
2022-01-17 17:43:59 -0600

    update build script

--- env_setup.sh
+++ env_setup.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 # Please replace package hashes with the ones on your installation. 
-module load gflags-2.2.2-gcc-10.3.0-svbilh4
-echo 'loaded gflags-2.2.2'
-module load intel-tbb-2020.3-gcc-10.3.0-kpcw75a
-echo 'loaded intel-tbb-2020.3'
-module load boost-1.76.0-gcc-10.3.0-6t5io7m
-echo 'loaded boost-1.76.0'
-#module load llvm-openmp-romp-mod-gcc-10.3.0-vccvbtz
-#echo 'loaded llvm-openmp-romp-mod'
-module load dyninst-11.0.0-gcc-10.3.0-sfbg3fy
-echo 'loaded dyninst-11.0.0'
-module load gcc-10.3.0-gcc-7.4.0-t6c5j3q
-echo 'loaded gcc-10.3.0'
-module load cmake-3.20.1-gcc-10.3.0-tuzc2ws
-echo 'loaded cmake-3.20'
-module load glog-0.4.0-gcc-10.3.0-srrufcz
-echo 'loaded glog-0.4.0'
+module load gcc-11.2.0-gcc-4.8.5-uvm7tcr
+module load llvm-13.0.0-gcc-11.2.0-qvadj4m
+module load cmake-3.22.1-gcc-11.2.0-gfvbai2
+module load glog-0.4.0-gcc-11.2.0-kuwbbcv
+module load dyninst-12.0.1-gcc-11.2.0-6uf3mbl
+module load gflags-2.2.2-gcc-11.2.0-waocagj
+module load boost-1.78.0-gcc-11.2.0-hyzaot3
+module load intel-tbb-2020.3-gcc-11.2.0-elgwscs
 echo "modules loaded"
+
+
 
