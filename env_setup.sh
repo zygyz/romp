2dea0c2ff695891868daa4ef4a9093b025d9b317
Yizi Gu <yizigu@gmail.com>
2022-01-17 18:32:12 -0600

    update build script

--- env_setup.sh
+++ env_setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # Please replace package hashes with the ones on your installation. 
 module load gcc-11.2.0-gcc-4.8.5-uvm7tcr
-module load llvm-13.0.0-gcc-11.2.0-qvadj4m
 module load cmake-3.22.1-gcc-11.2.0-gfvbai2
 module load glog-0.4.0-gcc-11.2.0-kuwbbcv
-module load dyninst-12.0.1-gcc-11.2.0-6uf3mbl
+module load dyninst-12.0.0-gcc-11.2.0-npexmmk
 module load gflags-2.2.2-gcc-11.2.0-waocagj
 module load boost-1.78.0-gcc-11.2.0-hyzaot3
 module load intel-tbb-2020.3-gcc-11.2.0-elgwscs
+module load llvm-openmp-12.0.1-gcc-11.2.0-jkvfg3u
 echo "modules loaded"
+
 
 
 
