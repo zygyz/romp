4506cad6981e4ad4aa76a0ef1731de22087b1c72
Yizi Gu <yizigu@gmail.com>
2022-01-29 13:55:46 -0600

    update install.sh to include different build mode

--- install.sh
+++ install.sh
@@ -4,7 +4,26 @@ rm -rf install
 mkdir build
 mkdir install
 cd build
-cmake -DCMAKE_CXX_FLAGS=-std=c++17 -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=`pwd`/../install ..
+build_type=$1
+if [ -z "${build_type}" ]; then
+  echo "default build type to RelWithDebInfo"
+  build_type="relwithdeb"
+fi
+
+echo "building and installing libromp with " $build_type
+
+if [ $build_type == "debug" ]; then
+  cmake -DCMAKE_CXX_FLAGS="-std=c++17 -g" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=`pwd`/../install ..
+fi
+
+if [ $build_type == "relwithdeb" ]; then
+  cmake -DCMAKE_CXX_FLAGS="-std=c++17 -g -O2" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=`pwd`/../install ..
+fi 
+
+if [ $build_type == "release" ]; then
+  cmake -DCMAKE_CXX_FLAGS="-std=c++17 -O3" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`pwd`/../install ..
+fi 
+
 make 
 make install
 cd ..
