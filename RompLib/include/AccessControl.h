d106fd5e4fd1be79c988f4e579fd50e8e8cf269b
Yizi Gu <yizigu@gmail.com>
2022-02-12 13:28:53 -0600

    [2/x] a wrapper function for reader writer lock upgrade

--- RompLib/include/AccessControl.h
+++ RompLib/include/AccessControl.h
@@ -10,3 +10,5 @@ private:
   mcs_lock_t* mLock;
   mcs_node_t* mNode;
 };
+
+bool upgradeFromReaderToWriter(pfq_rwlock-t* lock, pfq_rwlock_node_t* me, PerformanceCounters& performanceCounters);
--- RompLib/src/AccessControl.cpp
+++ RompLib/src/AccessControl.cpp
@@ -15,3 +15,13 @@ LockGuard::LockGuard(mcs_lock_t* lock, mcs_node_t* node, PerformanceCounters* pe
 LockGuard::~LockGuard() {
   mcs_unlock(mLock, mNode);
 }
+
+bool upgradeFromReaderToWriter(pfq_rwlock_t* lock, pfq_rwlock_node_t* me, PerformanceCounters& performanceCounters) {
+  auto lockUpgradeResult = pfq_rwlock_upgrade_from_read_to_write_lock(lock. me); 
+#ifdef PERFORMANCE 
+  if (lockUpgradeResult == upgraded_has_other_writer) {
+    gPerformanceCounters.bumpNumAccessHistoryContention();
+  }   
+#endif
+  return true; 
+}
