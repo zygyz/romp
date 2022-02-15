969f9317ed6b570d1b98bf0c54ac46c504b1d3ba
Yizi Gu <yizigu@gmail.com>
2022-02-15 00:12:38 -0600

    update performance.py for metric name

--- script/performance.py
+++ script/performance.py
@@ -15,17 +15,17 @@ skipped_benchmark_list = ['008', '024', '025', '137', '138', '031', '037', '038'
 
 KEY_NUM_CHECK_ACCESS_CALL="key_num_check_access_call";
 KEY_NUM_MEMORY_ACCESS_INSTRUMENTATION_CALL="key_num_memory_access_instrumentation_call";
-KEY_NUM_ACCESS_HISTORY_CONTENTION="key_num_access_history_contention";
-KEY_NUM_ACCESS_HISTORY_WRITE_WRITE_CONTENTION="key_num_access_history_write_write_contention";
-KEY_NUM_ACCESS_HISTORY_WRITE_READ_CONTENTION="key_num_access_history_write_read_contention";
-KEY_NUM_ACCESS_HISTORY_READ_WRITE_CONTENTION="key_num_access_history_read_write_contention";
+KEY_NUM_ACCESS_CONTORL_CONTENTION="key_num_access_control_contention";
+KEY_NUM_ACCESS_CONTROL_WRITE_WRITE_CONTENTION="key_num_access_control_write_write_contention";
+KEY_NUM_ACCESS_CONTROL_WRITE_READ_CONTENTION="key_num_access_control_write_read_contention";
+KEY_NUM_ACCESS_CONTROL_READ_WRITE_CONTENTION="key_num_access_control_read_write_contention";
 
 metrics_key_name_map = {
   KEY_NUM_CHECK_ACCESS_CALL: 'Check Access Function Call',
   KEY_NUM_ACCESS_HISTORY_CONTENTION: 'Access History Contention',
-  KEY_NUM_ACCESS_HISTORY_WRITE_WRITE_CONTENTION: 'Access History Write Write Contention',
-  KEY_NUM_ACCESS_HISTORY_WRITE_READ_CONTENTION: 'Access History Write Read Contention',
-  KEY_NUM_ACCESS_HISTORY_READ_WRITE_CONTENTION: 'Access History Read Write Contention', 
+  KEY_NUM_ACCESS_HISTORY_WRITE_WRITE_CONTENTION: 'Access Control Write Write Contention',
+  KEY_NUM_ACCESS_HISTORY_WRITE_READ_CONTENTION: 'Access Control Write Read Contention',
+  KEY_NUM_ACCESS_HISTORY_READ_WRITE_CONTENTION: 'Access Control Read Write Contention', 
 }
 
 def get_output_directory_path(benchmark_root_path: str, branch: str) -> str:
