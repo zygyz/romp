75a7143a192267d89c37844a7af01982135565de
Yizi Gu <yizigu@gmail.com>
2022-02-16 04:35:28 -0600

    temporarily print out processed data in list

--- script/visualize.py
+++ script/visualize.py
@@ -2,7 +2,6 @@
 
 import argparse
 import json
-import matplotlib.pyplot as plt
 import os
 import pprint
 import sys
@@ -29,30 +28,28 @@ def aggregate_data_for_metric(metric_name: str, data: dict) -> dict:
 def plot_performance_result(data: dict) -> None:
   for metric in metrics_config.metrics_key_list_for_visualize:
     result = aggregate_data_for_metric(metric, data);
-    draw(metric, result);
+    print('metric: ', metric);
+    pprint.PrettyPrinter(indent=2).pprint(result);
+    print('=======================================');
 
 def draw(metric_name: str, data: dict) -> None: 
-  print(data); 
-  values = data.values();
-  keys = data.keys();  
-  print(keys);
-  print(values);
   return; 
 
 def main() -> int:
   parser = argparse.ArgumentParser(description='Argument parsing for performance visualizer');
   parser.add_argument('performance_profile_path', type=str, help="path to performance profile json file");
   parser.add_argument('-p', '--pprint', action="store_true", help="parse the json file and pretty print to stdout");
-  parser.add_argument('-d', '--draw', action="store_true", help="parse the json file and plot the data");
+  #parser.add_argument('-d', '--draw', action="store_true", help="parse the json file and plot the data");
   args = parser.parse_args();
   data = parse_performance_profile(args.performance_profile_path); 
   if data is None:
     reutrn -1;
   if (args.pprint):
-    pprint.PrettyPrinter(indent=2).pprint(data);
-    return 0;
-  if (args.draw):
+    #pprint.PrettyPrinter(indent=2).pprint(data);
     plot_performance_result(data);
+    return 0;
+  #if (args.draw):
+  #  plot_performance_result(data);
   return 0;
 
 if __name__ == '__main__':
