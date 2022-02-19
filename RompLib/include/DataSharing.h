a7acae72fece7c09a13952941a400c1339c8dcd7
Yizi Gu <yizigu@gmail.com>
2022-02-19 02:54:57 -0600

    add numbering for data sharing enum

--- RompLib/include/DataSharing.h
+++ RompLib/include/DataSharing.h
@@ -4,16 +4,16 @@
 #include "TaskInfoQuery.h"
 
 enum DataSharingType { 
-  eNonThreadPrivate,
-  eThreadPrivateAccessOtherTask,
-  eThreadPrivateAccessCurrentTask,
-  eTaskExitFrameNotSet,
-  eTaskPrivate,
-  eNonWorkerThread,
-  eThreadMetaDataNotSet, 
-  eInitialThread,
-  eOmpRuntime,
-  eUndefined,
+  eNonThreadPrivate = 0,
+  eThreadPrivateAccessOtherTask = 1,
+  eThreadPrivateAccessCurrentTask = 2,
+  eTaskExitFrameNotSet = 3,
+  eTaskPrivate = 4,
+  eNonWorkerThread = 5,
+  eThreadMetaDataNotSet = 6, 
+  eInitialThread = 7,
+  eOmpRuntime = 8,
+  eUndefined = 9,
 };
 
 bool shouldCheckMemoryAccess(const ThreadInfo& threadInfo, const TaskMemoryInfo& taskMemoryInfo, const uint64_t memoryAddress, const ompt_frame_t* taskFrame);
