3d2bc04f09b725091f7c3f50b7149b15b283f880
Yizi Gu <yizigu@gmail.com>
2022-07-02 17:00:10 -0500

    comment out a log

--- RompLib/src/RompLib.cpp
+++ RompLib/src/RompLib.cpp
@@ -74,7 +74,7 @@ rollback: // will refactor to remove the tag. Using goto tag is actually more re
     // no access record, add current access to the record
     auto hasWriteWriteContention = guard.upgradeFromReaderToWriter();
     if (!hasWriteWriteContention || hasWriteWriteContention && !accessHistory->hasRecords()) {
-      RAW_DLOG(INFO, "add record to access history memory address: %lx in reduction %d is write: %d", curRecord.getCheckedMemoryAddress(), curRecord.isInReduction(), curRecord.isWrite());
+      //RAW_DLOG(INFO, "add record to access history memory address: %lx in reduction %d is write: %d", curRecord.getCheckedMemoryAddress(), curRecord.isInReduction(), curRecord.isWrite());
       accessHistory->addRecordToAccessHistory(curRecord);
       return false;
     } else {
