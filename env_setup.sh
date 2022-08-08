ad3528eee9b3b83040405f079deaef57bd8f892e
Yizi Gu <yizigu@gmail.com>
2022-08-08 12:32:04 -0500

    updated CMake file

--- env_setup.sh
+++ env_setup.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 # Please replace package hashes with the ones on your installation. 
-module load gcc-11.2.0-gcc-4.8.5-uvm7tcr
-module load cmake-3.22.1-gcc-11.2.0-gfvbai2
-module load glog-0.4.0-gcc-11.2.0-kuwbbcv
-module load dyninst-12.0.0-gcc-11.2.0-npexmmk
-module load gflags-2.2.2-gcc-11.2.0-waocagj
-module load boost-1.78.0-gcc-11.2.0-hyzaot3
-module load intel-tbb-2020.3-gcc-11.2.0-elgwscs
-module load llvm-openmp-12.0.1-gcc-11.2.0-jkvfg3u
-module load hwloc-2.7.0-gcc-11.2.0-tghphgv
-echo "modules loaded"
+module load gcc-11.2.0-gcc-4.8.5-hqbnirh
+echo "gcc loaded"
+module load cmake-3.23.2-gcc-11.2.0-n2257jc
+echo "cmake loaded"
+module load glog-0.4.0-gcc-11.2.0-wa2s6dm
+echo "glog loaded"
+module load dyninst-12.0.0-gcc-11.2.0-n6zlee5
+echo "dyninst loaded"
+module load gflags-2.2.2-gcc-11.2.0-orzzvx6
+echo "gflags loaded"
+module load boost-1.79.0-gcc-11.2.0-72pfeav
+echo "boost loaded"
+module load intel-tbb-2020.3-gcc-11.2.0-3y22way
+echo "intel-tbb loaded"
+module load hwloc-2.7.1-gcc-11.2.0-betflhl
+echo "hwloc loaded"
+module load llvm-openmp-12.0.1-gcc-11.2.0-a7b7xv6
+echo "llvm-openmp loaded"
+echo "all modules loaded"
+
+
+
+
 
 
 
